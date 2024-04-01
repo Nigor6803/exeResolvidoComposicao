641c521738ac41882f9a164a2c4a6846e3e0d529
Igor Nascimento <nigor6803@gmail.com>
2024-04-01 15:05:55 -0300

    exercico finalizado

--- src/aplicacao/Progama.java
+++ src/aplicacao/Progama.java
@@ -23,11 +23,11 @@ public class Progama {
 		String nomeDepartamento = edd.nextLine();
 		
 		System.out.println("FUNCIONARIO");
-		System.out.println("NOME:");
+		System.out.print("NOME:");
 		String nome = edd.nextLine();
-		System.out.println("NIVEL DO CARGO:");
+		System.out.print("NIVEL DO CARGO:");
 		String cargo = edd.nextLine();
-		System.out.println("SALARIO BASE:");
+		System.out.print("SALARIO BASE:");
 		double salarioBase = edd.nextDouble();
 		
 		Trabalhador trabalhador = new Trabalhador(nome, NivelTrabalhador.valueOf(cargo), salarioBase, new Departamento(nomeDepartamento));
@@ -37,14 +37,26 @@ public class Progama {
 		
 		for (int i = 1;i<=n;i++) {
 			System.out.println("CONTRATO #"+i);
-			System.out.println("DATA (DD/MM/AAAA)");
+			System.out.print("DATA (DD/MM/AAAA)");
 			Date dataContratual = sdf.parse(edd.next());
 			System.out.print("VALOR POR HORA:R$");
 			double valorPorHora = edd.nextDouble();
 			System.out.println("HORA:");
 			int hora = edd.nextInt();
 			ContratoHora contrato = new ContratoHora(dataContratual, valorPorHora, hora);
+			trabalhador.addContrato(contrato);
 		}
+		
+		System.out.println();
+		System.out.println("MÊS E ANO PARA CALCULAR SALARIO: (MM/YYYY)");
+		String mesEAno = edd.next();
+		int mes = Integer.parseInt(mesEAno.substring(0,2));
+		int ano = Integer.parseInt(mesEAno.substring(3));
+		
+		System.out.println("NOME:"+ trabalhador.getNome());
+		System.out.println("DEPARTAMENTO:"+ trabalhador.getDepartamento().getNome());
+		System.out.println("SALARIO FINAL DO "+mesEAno+":"+String.format("%.2f",trabalhador.renda(ano, mes)));
+
 		edd.close();
 	}
 
